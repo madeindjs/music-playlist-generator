8e01bb10edccbe9b153f12421195b6fa98719519
Alexandre Rousseau <alexandre@rsseau.fr>
2025-05-05 19:37:17 +0200

    fix: improve relative path export

--- lib.mjs
+++ lib.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import fs from "node:fs/promises";
-import { join } from "node:path";
+import { join, relative, sep } from "node:path";
 import { search } from "music-metadata-search";
 
 /**
@@ -28,12 +28,10 @@ export async function findExistingTags(path) {
  */
 function generateM3uPlaylist(tracks, title = "Playlist", path) {
   const rows = ["#EXTM3U", `#PLAYLIST:${title}`, ""];
-  for (const track of tracks)
-    rows.push(
-      `#EXTINF:0,${track.artist} - ${track.title}`,
-      track.path.replace(path, "."),
-      "",
-    );
+  for (const track of tracks) {
+    const trackPath = `.${sep}${relative(path, track.path)}`;
+    rows.push(`#EXTINF:0,${track.artist} - ${track.title}`, trackPath, "");
+  }
   return rows.join("\n");
 }
 
